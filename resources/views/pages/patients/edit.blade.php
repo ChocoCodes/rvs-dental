c1285df8515d2ca4d26ed90990aec79beffdbc61
Jul Leo Javellana <juljavellana1218@gmail.com>
2026-04-06 13:54:13 +0800

    chore: delete patient in edit form

--- resources/views/pages/patients/edit.blade.php
+++ resources/views/pages/patients/edit.blade.php
@@ -4,9 +4,19 @@
 @section('content')
     <div class="flex items-center justify-between py-4 md:py-8 mx-4 md:mx-8 border-b border-border sticky top-0 z-50 bg-background">
         <h1 class="text-2xl md:text-5xl">{{ $patient->first_name }} {{ $patient->last_name }}</h1>
-        <x-ui.button variant="secondary" class="rounded-xl text-xs md:text-sm">
-            <a href="{{ route('patients.index') }}">RETURN TO PATIENT LIST</a>
-        </x-ui.button>
+        <div class="flex gap-2 items-center">
+            <x-forms.container
+                action="{{ route('patients.destroy', $patient) }}"
+                onsubmit="return confirm('Are you sure you want to remove this patient? This process is irreversible.')"
+                method="POST"
+            >
+                @method("DELETE")
+                <x-ui.button type="submit" variant="danger" class="rounded-xl text-xs md:text-sm">REMOVE PATIENT</x-ui.button>
+            </x-forms.container>
+            <x-ui.button variant="secondary" class="rounded-xl text-xs md:text-sm">
+                <a href="{{ route('patients.index') }}">RETURN TO PATIENT LIST</a>
+            </x-ui.button>
+        </div>
     </div>
 
     @include('pages.patients.partials.form', [
